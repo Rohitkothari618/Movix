581c787e648b4e3f4c966949fdd04af50d06bd94
Rohit kothari <rohitkothari@Rohits-MacBook-Pro.local>
2023-03-03 14:54:40 +0530

    Api Succesfully Taken

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from 'react';
+import { fetchDataFromApi } from './utils/api';
+
+function App() {
+  useEffect(() => {
+    apiTesting();
+  }, []);
+  const apiTesting = () => {
+    fetchDataFromApi('/movie/popular').then((res) => {
+      console.log(res);
+    });
+  };
+  return <div className='App'>Helloworld</div>;
+}
+
+export default App;
